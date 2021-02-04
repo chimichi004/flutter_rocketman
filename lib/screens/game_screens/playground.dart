af784ce39a67844401da35e682aa5ba9ffc8bd35
chimichi004 <chember004@gmail.com>
2021-02-05 00:15:43 +0800

    add a dynamic background and add moving character

--- lib/screens/game_screens/playground.dart
+++ lib/screens/game_screens/playground.dart
@@ -2,32 +2,42 @@ import 'dart:ui';
 
 import 'package:flutter/src/gestures/tap.dart';
 import 'package:flutter_rocketman/screens/base.dart';
+import 'package:flutter_rocketman/screens/game/player.dart';
 import 'package:flutter_rocketman/screens/util/background.dart';
+import 'package:flutter_rocketman/screens/util/dynamic_background.dart';
 
 class PlayGround extends BaseWidget {
-  Background _bg;
+  BaseWidget _bg;
+  Player _player;
 
   PlayGround() {
-    _bg = Background('playground/sky1.png');
+    _bg = DynamicBackground(5, 'playground/sky.png');
+    _player = Player();
   }
 
   @override
   void render(Canvas canvas) {
     //
     _bg.render(canvas);
+    _player.render(canvas);
   }
 
   @override
   void resize(Size size) {
     //
     _bg.resize(size);
+    _player.resize(size);
   }
 
   @override
   void update() {
-    // TODO: implement update
+    //
+    _player.update();
+    _bg.update();
   }
 
   @override
-  void onTapDown(TapDownDetails detail, Function fn) {}
+  void onTapDown(TapDownDetails detail, Function fn) {
+    _player.onTapDown(detail, () {});
+  }
 }
